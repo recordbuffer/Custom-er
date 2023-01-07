9f1044fddf4e44d6b423c78f068e28158e9014d5
recordbuffer <76642597+recordbuffer@users.noreply.github.com>
2023-01-07 14:29:27 +0900

    Add Backend setup

---
+++ src/main/java/com/shop/customer/CustomErApplication.java
@@ -0,0 +1,13 @@
+package com.shop.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomErApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomErApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/customer/CustomErApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomErApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
