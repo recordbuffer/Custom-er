4812f965f897c59832f340054e1ec236eb73550d
recordbuffer <76642597+recordbuffer@users.noreply.github.com>
2023-01-08 14:01:26 +0900

    Add vue-router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import Home from "@/pages/Home";
+import Login from "@/pages/Login";
 
-Vue.config.productionTip = false
+const routes = [
+  {path: '/', component: Home},
+  {path: '/login', component: Login}
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
