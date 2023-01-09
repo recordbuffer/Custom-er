6d7f4cb10a2a41ab737d21959b3ab65adb16435e
recordbuffer <76642597+recordbuffer@users.noreply.github.com>
2023-01-09 10:35:14 +0900

    Update Header.vue

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -10,24 +10,22 @@
           <li class="nav-item dropdown">
             <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">문구</button>
             <ul class="dropdown-menu dropdown-menu-dark">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li><a class="dropdown-item" href="#">노트</a></li>
+              <li><a class="dropdown-item" href="#">스티커</a></li>
             </ul>
           </li>
           <li class="nav-item dropdown">
             <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">생활</button>
             <ul class="dropdown-menu dropdown-menu-dark">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li><a class="dropdown-item" href="#">컵</a></li>
+              <li><a class="dropdown-item" href="#">티셔츠</a></li>
             </ul>
           </li>
         </ul>
 
         <div class="text-end">
           <button type="button" class="btn btn-outline-light me-2">Login</button>
-          <button type="button" class="btn btn-light">Sign-up</button>
+          <button type="button" class="btn btn-outline-light" style="background-color: #FC5030">Sign-up</button>
         </div>
       </div>
     </div>
